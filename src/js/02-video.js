2060965c00314321a58471b2e40b0091742716ff
NCatalin93 <catalin.nedelcu93@gmail.com>
2024-01-24 18:12:29 +0200

    add task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
+
+const key = 'videoplayer-current-time';
+
+const onPlay = function (event) {
+  localStorage.setItem(key, event.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem(key));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,4 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const key = 'feedback-form-state';
