cdb17c12a0870bc99b6e09e5825d211c18e42f47
NCatalin93 <catalin.nedelcu93@gmail.com>
2024-01-25 14:23:24 +0200

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,42 @@
 import throttle from 'lodash.throttle';
-
-const feedbackForm = document.querySelector('.feedback-form');
 const key = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const loadForm = () => {
+  const storedForm = localStorage.getItem(key);
+  if (storedForm) {
+    const { email, message } = JSON.parse(storedForm);
+    emailInput.value = email;
+    messageInput.value = message;
+  }
+};
+
+const formInput = throttle(() => {
+  const formInfo = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(key, JSON.stringify(formInfo));
+}, 500);
+
+const formSubmit = event => {
+  event.preventDefault();
+  const savedObject = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  console.log(savedObject);
+
+  localStorage.removeItem(key);
+  emailInput.value = '';
+  messageInput.value = '';
+};
+
+form.addEventListener('input', formInput);
+form.addEventListener('submit', formSubmit);
+
+loadForm();
